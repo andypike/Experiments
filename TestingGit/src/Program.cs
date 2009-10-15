2c7da22c193bf43fdba40969e9e6b76400ebb974
Andy Pike <andy@andypike.com>
2009-10-15 23:16:03 +0100

    added user name and restructuring

---
+++ TestingGit/src/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace TestingGit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What's your name?");
+            string name = Console.ReadLine();
+
+            Console.WriteLine("Hello, " + name);
+            Console.ReadLine();
+        }
+    }
+}
